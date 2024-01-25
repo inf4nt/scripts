44f8998225b40cc34b454c984e363d01a2a1cae7
Maksym Lukaretskyi <com.infant@gmail.com>
2024-01-25 22:22:19 +0200

    updates

--- graalvm-21-install.sh
+++ graalvm-21-install.sh
@@ -1,18 +1,23 @@
+WORK_DIR="/opt"
+GRAALVM_ARCHIVE="$WORK_DIR/graalvm-jdk.tar.gz"
+
+GRAALVM_DOWNLOAD_URL="https://download.oracle.com/graalvm/21/latest/graalvm-jdk-21_linux-x64_bin.tar.gz"
+
 sudo apt update -y
 sudo apt upgrade -y
 
 sudo apt install libfl-dev zlib1g-dev curl -y
 
-sudo curl -o /opt/graalvm-jdk-21_linux-x64_bin.tar.gz https://download.oracle.com/graalvm/21/latest/graalvm-jdk-21_linux-x64_bin.tar.gz
+sudo curl -o $GRAALVM_ARCHIVE $GRAALVM_DOWNLOAD_URL
 
-sudo tar -xzf /opt/graalvm-jdk-21_linux-x64_bin.tar.gz -C /opt/
+sudo tar -xzf $GRAALVM_ARCHIVE -C $WORK_DIR
 
-sudo rm /opt/graalvm-jdk-21_linux-x64_bin.tar.gz
+sudo rm $GRAALVM_ARCHIVE
 
-GRAALVM_PATH=$(find /opt/ -type d -name 'graalvm-jdk-21*' -printf "%p" -quit)
+GRAALVM_HOME=$(find $WORK_DIR -type d -name 'graalvm-jdk-21*' -printf "%p" -quit)
 
-echo JAVA_HOME=$GRAALVM_PATH >> ~/.bashrc
+echo JAVA_HOME=$GRAALVM_HOME >> ~/.bashrc
 echo 'PATH="$JAVA_HOME/bin:$PATH"' >> ~/.bashrc
 
-echo export JAVA_HOME=$GRAALVM_PATH >> ~/.bashrc
+echo export JAVA_HOME=$GRAALVM_HOME >> ~/.bashrc
 echo 'export PATH="$JAVA_HOME/bin:$PATH"' >> ~/.bashrc
--- mvn-install.sh
+++ mvn-install.sh
@@ -1,13 +1,23 @@
+WORK_DIR="/opt"
+MAVEN_VERSION="3.9.6"
+MAVEN_ARCHIVE="$WORK_DIR/apache-maven.tar.gz"
+
+MAVEN_HOME="$WORK_DIR/apache-maven-$MAVEN_VERSION"
+MAVEN_DOWNLOAD_URL="https://dlcdn.apache.org/maven/maven-3/$MAVEN_VERSION/binaries/apache-maven-$MAVEN_VERSION-bin.tar.gz"
+
+sudo apt update -y
+sudo apt upgrade -y
+
 sudo apt install curl -y
 
-sudo curl -o /opt/apache-maven-3.9.6-bin.tar.gz https://dlcdn.apache.org/maven/maven-3/3.9.6/binaries/apache-maven-3.9.6-bin.tar.gz
+sudo curl -o $MAVEN_ARCHIVE $MAVEN_DOWNLOAD_URL
 
-sudo tar -xzf /opt/apache-maven-3.9.6-bin.tar.gz -C /opt/
+sudo tar -xzf $MAVEN_ARCHIVE -C $WORK_DIR
 
-sudo rm /opt/apache-maven-3.9.6-bin.tar.gz
+sudo rm $MAVEN_ARCHIVE
 
-echo M2_HOME=/opt/apache-maven-3.9.6/ >> ~/.bashrc
+echo M2_HOME=$MAVEN_HOME >> ~/.bashrc
 echo 'PATH="$M2_HOME/bin:$PATH"' >> ~/.bashrc
 
-echo export M2_HOME=/opt/apache-maven-3.9.6/ >> ~/.bashrc
+echo export M2_HOME=$MAVEN_HOME >> ~/.bashrc
 echo 'export PATH="$M2_HOME/bin:$PATH"' >> ~/.bashrc
