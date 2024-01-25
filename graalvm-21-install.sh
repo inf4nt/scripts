ab68c2bdd4e168d0691ac88e37bff4b3fd08bd4d
Maksym Lukaretskyi <com.infant@gmail.com>
2024-01-25 20:05:36 +0200

    updates

--- graalvm-21-install.sh
+++ graalvm-21-install.sh
@@ -7,12 +7,12 @@ sudo curl -o /opt/graalvm-jdk-21_linux-x64_bin.tar.gz https://download.oracle.co
 
 sudo tar -xzf /opt/graalvm-jdk-21_linux-x64_bin.tar.gz -C /opt/
 
-sudo rm -rf /opt/graalvm-jdk-21_linux-x64_bin.tar.gz
+sudo rm /opt/graalvm-jdk-21_linux-x64_bin.tar.gz
 
 _java_home_path=$(find /opt/ -type d -name 'graalvm-jdk-21*' -printf "%p" -quit)
 
-echo JAVA_HOME=${_java_home_path} >> ~/.bashrc
+echo JAVA_HOME=$_java_home_path >> ~/.bashrc
 echo 'PATH="$JAVA_HOME/bin:$PATH"' >> ~/.bashrc
 
-echo export JAVA_HOME=${_java_home_path} >> ~/.bashrc
+echo export JAVA_HOME=$_java_home_path >> ~/.bashrc
 echo 'export PATH="$JAVA_HOME/bin:$PATH"' >> ~/.bashrc
---
+++ m2-share.sh
@@ -0,0 +1,5 @@
+echo 'Enter windows username'
+
+read username
+
+ln -s /mnt/c/Users/$username/.m2 ~/
