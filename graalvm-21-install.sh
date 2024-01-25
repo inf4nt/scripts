189d887c0972dbce30562d78bc44d7f2cebde347
Maksym Lukaretskyi <com.infant@gmail.com>
2024-01-25 21:33:29 +0200

    updates

--- graalvm-21-install.sh
+++ graalvm-21-install.sh
@@ -9,10 +9,10 @@ sudo tar -xzf /opt/graalvm-jdk-21_linux-x64_bin.tar.gz -C /opt/
 
 sudo rm /opt/graalvm-jdk-21_linux-x64_bin.tar.gz
 
-_java_home_path=$(find /opt/ -type d -name 'graalvm-jdk-21*' -printf "%p" -quit)
+GRAALVM_PATH=$(find /opt/ -type d -name 'graalvm-jdk-21*' -printf "%p" -quit)
 
-echo JAVA_HOME=$_java_home_path >> ~/.bashrc
+echo JAVA_HOME=$GRAALVM_PATH >> ~/.bashrc
 echo 'PATH="$JAVA_HOME/bin:$PATH"' >> ~/.bashrc
 
-echo export JAVA_HOME=$_java_home_path >> ~/.bashrc
+echo export JAVA_HOME=$GRAALVM_PATH >> ~/.bashrc
 echo 'export PATH="$JAVA_HOME/bin:$PATH"' >> ~/.bashrc
