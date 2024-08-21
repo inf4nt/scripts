b5a43a8c71c26cddfb29b8573ae24b03fcf56a19
Lukaretskyi Maksym <com.infant@gmail.com>
2024-08-22 00:24:55 +0300

    openjdk17

---
+++ wsl/openjdk-17-install.sh
@@ -0,0 +1,23 @@
+WORK_DIR="/opt"
+ARCHIVE_ABSOLUTE_PATH="$WORK_DIR/openjdk-17.0.2.tar.gz"
+DOWNLOAD_URL="https://download.java.net/java/GA/jdk17.0.2/dfd4a8d0985749f896bed50d7138ee7f/8/GPL/openjdk-17.0.2_linux-x64_bin.tar.gz"
+JAVA_HOME_ABSOLUTE_PATH="$WORK_DIR/jdk-17.0.2"
+
+sudo apt update -y
+sudo apt upgrade -y
+
+sudo apt install curl -y
+
+sudo curl -o $ARCHIVE_ABSOLUTE_PATH $DOWNLOAD_URL
+
+sudo tar -xzf $ARCHIVE_ABSOLUTE_PATH -C $WORK_DIR
+
+sudo rm $ARCHIVE_ABSOLUTE_PATH
+
+echo '# JAVA VARIABLES' >> ~/.bashrc
+echo JAVA_HOME=$JAVA_HOME_ABSOLUTE_PATH >> ~/.bashrc
+echo 'PATH="$JAVA_HOME/bin:$PATH"' >> ~/.bashrc
+
+echo export JAVA_HOME=$JAVA_HOME_ABSOLUTE_PATH >> ~/.bashrc
+echo '# JAVA VARIABLES END' >> ~/.bashrc
+echo '' >> ~/.bashrc
